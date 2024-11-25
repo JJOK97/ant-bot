0aa1ea2632c817bd237b2ccd91e1178480eceddf
JinSeok <spdlqjanf@naver.com>
2024-11-25 17:41:22 +0900

    [feat] mvp 폴더 생성 및 의존성 주입(#1)

---
+++ MVP/src/test/java/com/antbot/mvp/MvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antbot.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
